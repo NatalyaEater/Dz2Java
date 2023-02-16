cc48938ed36f960b10d36c0eb130ca7c0f208dd1
Natasha Eater <natalya.iter@yandex.ru>
2023-02-16 16:46:33 +0300

    dz2

---
+++ task2.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class task2 {
+    public static void main(String[] args) {
+        int[] arr = { 1, 17, 23, 3, 4, 8, 20, 17, 11, 10, 1, 9, 7, 5, 5, 5, 7, 6, 8 };
+        Logger logger = Logger.getLogger(task2.class.getName());
+        FileHandler fh;
+        try {
+            fh = new FileHandler("log.txt");
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+            int n = arr.length;
+
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            logger.info("Sorted array" + Arrays.toString(arr));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
